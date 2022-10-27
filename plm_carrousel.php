f2de94cecf15210a2e5d9fc3cfc72ce5cfcac112
Moisan <e2194803@cmaisonneuve.qc.ca>
2022-10-27 16:12:09 -0400

    c9 Ajout d une boîte modale dans le fichier carrousel.js

--- plm_carrousel.php
+++ plm_carrousel.php
@@ -2,15 +2,15 @@
 /**
  * @package           Carrousel
  * @author            Pierre-Luc Moisan
- * @copyright         Collège de Maisonneuve
+ * @copyright         Collèege de Maisonneuve
  * @license           GPL-2.0-or-later
  * 
- * Plugin Name: plm_carrousel
- * Description: Permet d'afficher une boîte modale de l'image sélectionnée d'une galerie. Pour ensuite accéder à toutes les autres images de la galerie.
- * Author: Pierre-Luc Moisan
+ * plugin Name: em_carrousel
+ * Description: Permet d'afficher une boîte modale de l'image sélectionnée d'une galerie. Pour ensuite accéder à toutes les images de la galerie
+ * Author: Eddy Martin
  * Author URI: https://github.com/plm333
  */
-
+ 
  /*
 filemtime() // retourne en milliseconde le temps de la dernière sauvegarde
 plugin_dir_path() // retourne le chemin du répertoire du plugin
@@ -20,44 +20,42 @@ wp_enqueue_script() // intègre le script dans la page
 wp_enqueue_scripts // le hook
 */
 
-function plm_enqueue(){
-    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
-    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
-
-    wp_enqueue_style("plm_carrousel",
-                    plugin_dir_url(__FILE__) . "style.css",
-                    array(),
-                    $version_css,
-                    false);
-
-    wp_enqueue_script("plm_carrousel",
-                    plugin_dir_url(__FILE__) . "js/carrousel.js",
-                    array(),
-                    $version_js,
-                    true);                
-
-}
-
-add_action("wp_enqueue_scripts","plm_enqueue");
 
  
-function genere_html(){
+function em_enqueue()
+{
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js =  filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+ 
+wp_enqueue_style('em_carrousel_css',
+                plugin_dir_url(__FILE__) . "style.css",
+                array(),
+                $version_css,
+                false);
+ 
+wp_enqueue_script('em_carrousel_js',
+                plugin_dir_url(__FILE__) . "js/carrousel.js",
+                array(),
+                $version_js,
+                true);                
+}
+ 
+add_action("wp_enqueue_scripts","em_enqueue");
+
+
+ 
+function boite_carrousel(){
     /////////////////////////////////////// HTML
     // Le conteneur d'une boîte
     $contenu = 
-    "<setion class='carrousel'>"
-    . '<button class="carrousel--fermer">X</button>'
-    . '<figure class="carrousel__figure"></figure>'
-    . '<form class="carrousel__form"></form>'
-    . '</section> <!-- fin class="carrousel" -->';
+    "<button class='bouton'>Ouvrir carrousel</button>"
+     ."<div class='carrousel'>"
+        .'<button class="carrousel__x">X<button>'
+        .'<figure class="carrousel__figure"></figure>'
+        .'<form class="carrousel__form"></form>'
+    . '</div> <!-- fin class="carrousel" -->';
     
     return $contenu;
    }
+ 
    add_shortcode('plm_carrousel', 'boite_carrousel');
-
-
-
-
-?>
-
-
