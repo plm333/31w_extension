9fce8718abe71bf29058bc6ec2f20c860146e481
Moisan <e2194803@cmaisonneuve.qc.ca>
2022-10-27 15:00:43 -0400

    c9 Ajout de la function plm_enqueue() dans le fichier plm_carrousel.php

--- plm_carrousel.php
+++ plm_carrousel.php
@@ -11,6 +11,34 @@
  * Author URI: https://github.com/plm333
  */
 
+ /*
+filemtime() // retourne en milliseconde le temps de la dernière sauvegarde
+plugin_dir_path() // retourne le chemin du répertoire du plugin
+__FILE__ // le fichier en train de s'exécuter
+wp_enqueue_style() // Intègre le link:css dans la page
+wp_enqueue_script() // intègre le script dans la page
+wp_enqueue_scripts // le hook
+*/
+
+function plm_enqueue(){
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+    wp_enqueue_style("plm_carrousel",
+                    plugin_dir_url(__FILE__) . "style.css",
+                    array(),
+                    $version_css,
+                    false);
+
+    wp_enqueue_script("plm_carrousel",
+                    plugin_dir_url(__FILE__) . "js/carrousel.js",
+                    array(),
+                    $version_js,
+                    true);                
+
+}
+
+add_action("wp_enqueue_scripts","plm_enqueue");
 
  
 function genere_html(){
