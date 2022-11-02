54f93b274bb0ebea53ed29edb77689bae8cc4c04
Moisan <e2194803@cmaisonneuve.qc.ca>
2022-11-02 14:05:31 -0400

    c10 Ajout de radio button dans le carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,4 +1,5 @@
 (function(){
+
     console.log("carrousel")
     let elmGalerie = document.querySelector('.galerie');
     let elmGalerieImg = document.querySelectorAll('.galerie img');
@@ -6,23 +7,45 @@
     let elmCarrousel = document.querySelector('.carrousel');
     let elmCarrousel__x = document.querySelector('.carrousel__x');
     let elmCarrousel__figure = document.querySelector('.carrousel__figure');
-
+    let elmCarrousel__form = document.querySelector('.carrousel__form');
+    let index = 0;
+    let dernierIndex = -1;
 
     elmBouton.addEventListener('mousedown', function(){
         console.log("ouverture du carrousel");
         elmCarrousel.classList.add('carrousel--ouvrir');
         for (const elmImg of elmGalerieImg){
-            console.log(elmImg.getAttribute('src'));
-            let elmCarrouselImg = document.createElement('img');
-            //console.log(elmImg.getAttribute('src'));
-            elmCarrouselImg.setAttribute('src', elmImg.getAttribute('src'));
-            elmCarrouselImg.classList.add('carrousel__figure__img');
-            elmCarrousel__figure.appendChild(elmCarrouselImg);
+            ajout_img_dans_carrousel(elmImg);
+            ajout_radio_dans_carrousel();
         }
-
     })
+
+    function ajout_img_dans_carrousel(elmImg) {
+        console.log(elmImg.getAttribute('src'));
+        let elmCarrouselImg = document.createElement('img');
+        //console.log(elmImg.getAttribute('src'));
+        elmCarrouselImg.setAttribute('src', elmImg.getAttribute('src'));
+        elmCarrouselImg.classList.add('carrousel__figure__img');
+        elmCarrousel__figure.appendChild(elmCarrouselImg);
+    }
+
+    function ajout_radio_dans_carrousel() {
+        let elmCarrousel__form__radio = document.createElement('input');
+        elmCarrousel__form__radio.setAttribute('type', 'radio');
+        elmCarrousel__form__radio.classList.add('carrousel__form__rad');
+        elmCarrousel__form__radio.setAttribute('name', 'carrousel__form__rad');
+        elmCarrousel__form__radio.dataset.index = index;
+        index ++;
+        elmCarrousel__form.appendChild(elmCarrousel__form__radio);
+
+        elmCarrousel__form__radio.addEventListener('mousedown', function (){
+            console.log();
+        })
+
+    }
 
     elmCarrousel__x.addEventListener('mousedown', function(){
         elmCarrousel.classList.remove('carrousel--ouvrir');
     })
+
 })()
